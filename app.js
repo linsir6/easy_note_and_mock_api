c42c68c55172799940d433c079faf9af46ea5ecb
linsir <linsir678@gmail.com>
2017-07-27 17:13:01 +0800

    实现了最基础的mock的功能

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const Koa = require('koa');
 const app = new Koa();
 var controller = require('koa-route');
+var service = require('./service.js');
 
 
 
-// response
-// app.use(ctx => {
-//   ctx.body = 'Hello Koa';
-// });
 
-var address = '/zzz';
-
-app.use(controller.get(address, function*() {
+app.use(controller.get('/', function*() {
     this.set('Cache-Control', 'no-cache');
-    this.body = "zz";
+    var list = yield service.select_all_api();
+
+    
+    function generateRouteFn(){
+            let rowData = list[i];
+            return function*() {
+              this.set('Cache-Control', 'no-cache');
+              this.body = rowData;
+            }
+    }
+    for(var i = 0;i < list.length; i++){
+
+          console.log(list[0]);
+          app.use(controller.get(list[i].address, generateRouteFn()));
+          console.log("这里有执行到");
+
+    }
+
+
+    this.body = yield service.select_all_api();
 }));
 
 console.log("服务已经开启～");
